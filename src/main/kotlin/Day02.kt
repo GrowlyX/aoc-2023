fbd45f4b39827f6e985110e417bc73882beaea3c
GrowlyX <growlyxdev@gmail.com>
2023-12-02 14:55:46 -0500

    feat: day 2

---
+++ src/main/kotlin/Day02.kt
@@ -0,0 +1,70 @@
+import kotlin.time.measureTimedValue
+
+fun main()
+{
+    val nonNumeric = "[^0-9]".toRegex()
+    val numeric = "[^a-z]".toRegex()
+
+    val limits = mapOf("red" to 12, "green" to 13, "blue" to 14)
+
+    fun List<String>.parseInto() =
+        map {
+            val gameID = it.indexOf(':')
+            it.substring(0..<gameID) to it
+                .substring(gameID + 2)
+                .split("; ")
+                .map { group ->
+                    group.split(", ")
+                }
+        }
+        .map {
+            it.first.removePrefix("Game ").toInt() to
+                it.second.map { group ->
+                    group.map { dice ->
+                        dice.replace(nonNumeric, "").toInt() to
+                            dice.replace(numeric, "")
+                    }
+                }
+        }
+
+    fun part1(input: List<String>) = input.parseInto()
+        .filter { mappings ->
+            mappings.second.all { group ->
+                group.none { it.first > limits[it.second]!! }
+            }
+        }
+        .sumOf { it.first }
+
+    fun part2(input: List<String>) = input.parseInto()
+        .sumOf {
+            val minimums = mutableMapOf(
+                "red" to -1,
+                "green" to -1,
+                "blue" to -1
+            )
+
+            for (pair in it.second.flatten())
+            {
+                if (minimums[pair.second]!! < pair.first)
+                {
+                    minimums[pair.second] = pair.first
+                }
+            }
+
+            minimums.values.reduce { acc, i ->
+                if (i == -1)
+                    return@reduce acc
+
+                (if (acc == 0) 1 else acc) * i
+            }
+        }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day2", "test")
+    check(part1(testInput).println() == 8)
+    check(part2(testInput).println() == 2286)
+
+    val input = readInput("Day2", "input")
+    measureTimedValue { part1(input) }.println()
+    measureTimedValue { part2(input) }.println()
+}
