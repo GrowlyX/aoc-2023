0eec99a84ce21d229b4e3d6fc5dc1ad9bce8ec9f
GrowlyX <growlyxdev@gmail.com>
2023-12-01 19:43:22 -0500

    feat: day 1

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,15 +1,50 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+import kotlin.math.min
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+fun main()
+{
+    val numbers = listOf(
+        "one" to 1, "two" to 2, "three" to 3,
+        "four" to 4, "five" to 5, "six" to 6,
+        "seven" to 7, "eight" to 8, "nine" to 9
+    )
+
+    fun part1(input: List<String>) = input
+        .map { it.toCharArray().filter { char -> char.isDigit() } }
+        .sumOf {
+            String(charArrayOf(it.first(), it.last())).toInt()
+        }
+
+    fun part2(input: List<String>) = input
+        .sumOf { word ->
+            var finalString = ""
+            word.forEachIndexed { index, char ->
+                if (char.isDigit())
+                {
+                    finalString += char
+                    return@forEachIndexed
+                }
+
+                for (number in numbers)
+                {
+                    val embedded = word.substring(
+                        index,
+                        min(index + number.first.length, word.length)
+                    )
+
+                    if (embedded == number.first)
+                    {
+                        finalString += number.second
+                        return@forEachIndexed
+                    }
+                }
+            }
+
+            "${finalString.first()}${finalString.last()}".toInt()
+        }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day1", "test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 281)
 
     val input = readInput("Day1", "input")
     part1(input).println()
