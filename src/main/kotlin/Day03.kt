46c35eac1bc5e055cf7ad17af8fe04b1347f97cc
GrowlyX <growlyxdev@gmail.com>
2023-12-03 22:07:08 -0500

    feat: day 3

---
+++ src/main/kotlin/Day03.kt
@@ -0,0 +1,75 @@
+import java.lang.ref.WeakReference
+import kotlin.time.measureTimedValue
+
+fun main()
+{
+    data class Part(
+        val region: Region2D,
+        val value: Int
+    )
+
+    val numberExtractor = "[1-9]\\d*".toRegex()
+    fun List<String>.mapIntoPlaneAndParts() = with(with(this) {
+        val pointMappings = mutableMapOf<CharPoint, Char>()
+        forEachIndexed { index, schematic ->
+            schematic.forEachIndexed { charIndex, char ->
+                pointMappings[CharPoint(index, charIndex)] = char
+            }
+        }
+
+        CharPlane2D(points = pointMappings)
+    }) {
+        this to map { numberExtractor.findAll(it).toList() }
+            .mapIndexed { index, results ->
+                results.map {
+                    Part(
+                        region = Region2D(
+                            point1 = CharPoint(index, it.range.first),
+                            point2 = CharPoint(index, it.range.last),
+                            plane = WeakReference(this)
+                        ),
+                        value = it.value.toInt()
+                    )
+                }
+            }
+            .flatten()
+    }
+
+    fun part1(input: List<String>) = with(input.mapIntoPlaneAndParts()) {
+        second
+            .filterNot {
+                val bordering = first.bordering(it.region)
+
+                bordering.all { point ->
+                    val value = point.pointAt(first)
+                    value.isLetterOrDigit() || value == '.'
+                }
+            }
+            .sumOf { it.value }
+    }
+
+    fun part2(input: List<String>) = with(input.mapIntoPlaneAndParts()) {
+        val gears = mutableMapOf<CharPoint, List<Int>>()
+        second.forEach {
+            val bordering = first.bordering(it.region)
+            bordering
+                .filter { gear -> gear.pointAt(first) == '*' }
+                .forEach { gear ->
+                    gears.compute(gear) { _, parts -> (parts ?: listOf()) + it.value }
+                }
+        }
+
+        gears.values
+            .filter { it.size == 2 }
+            .sumOf { it.first() * it.last() }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day3", "test")
+    println(part1(testInput).println())
+    println(part2(testInput).println())
+
+    val input = readInput("Day3", "input")
+    measureTimedValue { part1(input) }.println()
+    measureTimedValue { part2(input) }.println()
+}
--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,3 +1,4 @@
+import java.lang.ref.WeakReference
 import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
@@ -19,3 +20,60 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun <T> T.println() = apply { println(this) }
+
+data class CharPoint(
+    val x: Int,
+    val y: Int
+)
+{
+    fun pointAt(plane2D: CharPlane2D) = plane2D.points[this] ?: '.'
+}
+
+data class CharPlane2D(
+    val points: Map<CharPoint, Char>
+)
+{
+    fun expand(region: Region2D, expandsBy: Int): Region2D
+    {
+        val minX = minOf(region.point1.x, region.point2.x) - expandsBy
+        val minY = minOf(region.point1.y, region.point2.y) - expandsBy
+        val maxX = maxOf(region.point1.x, region.point2.x) + expandsBy
+        val maxY = maxOf(region.point1.y, region.point2.y) + expandsBy
+
+        val expandedPoint1 = CharPoint(minX, minY)
+        val expandedPoint2 = CharPoint(maxX, maxY)
+
+        return Region2D(expandedPoint1, expandedPoint2, WeakReference(this))
+    }
+
+    fun bordering(region: Region2D) = expand(region, expandsBy = 1)
+        .toList()
+        .filterNot(region::coercesIn)
+}
+
+data class Region2D(
+    val point1: CharPoint,
+    val point2: CharPoint,
+    val plane: WeakReference<CharPlane2D>
+) : Iterable<CharPoint>
+{
+    private val minX = minOf(point1.x, point2.x)
+    private val maxX = maxOf(point1.x, point2.x)
+    private val minY = minOf(point1.y, point2.y)
+    private val maxY = maxOf(point1.y, point2.y)
+
+    private val xRange = minX..maxX
+    private val yRange = minY..maxY
+
+    fun coercesIn(point: CharPoint) = point.x in xRange && point.y in yRange
+
+    override fun iterator() = iterator {
+        for (x in xRange)
+        {
+            for (y in yRange)
+            {
+                yield(CharPoint(x, y))
+            }
+        }
+    }
+}
